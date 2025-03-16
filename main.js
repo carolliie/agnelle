6f83457e4a59d91086ae49adbee8750889a36107
carolliie <anacvieira1415@gmail.com>
2025-03-16 00:20:44 -0300

    fix: carossel & banner de desconto

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    new Glider(document.querySelector('.glider'), {
-        settingName: settingValue
-    });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("loaded");
 });
@@ -45,36 +39,5 @@ document.addEventListener("DOMContentLoaded", () => {
             bar.classList.toggle("active");
         });
     })
-})
-
-
-new Glider(document.querySelector('.glider'), {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    draggable: true,
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    },
-    responsive: [
-        {
-            // screens greater than >= 775px
-            breakpoint: 775,
-            settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 'auto',
-                slidesToScroll: 'auto',
-                duration: 0.25
-            }
-        }, {
-            // screens greater than >= 1024px
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                duration: 1
-            }
-        }
-    ]
 });
 
